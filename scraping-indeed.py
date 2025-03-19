20d45a0bb0926e5015fb13e71212d9ee1e941646
Helder-Souza <helder825@hotmail.com>
2025-03-19 19:30:59 -0300

    add write csv def

--- scraping-indeed.py
+++ scraping-indeed.py
@@ -1,6 +1,7 @@
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 import time
+import csv
 
 def scrap():
     url = "https://br.indeed.com/jobs?q=desenvolvedor+java&l=remoto"
@@ -18,17 +19,28 @@ def scrap():
     jobs = driver.find_elements(By.CLASS_NAME, "jobTitle")
     print(f"Encotradas {len(jobs)} vagas!")
 
+    vagas = []
+
     for job in jobs:
         try:
             link_element = job.find_element(By.TAG_NAME, "a")
             job_title = link_element.text.strip()
             job_link = link_element.get_attribute("href")
+            vagas.append([job_title, job_link])
             print(f"Título: {job_title}")
             print(f"Link: {job_link}\n")
         except Exception as e:
             print(f"Erro ao processar vaga: {e}")
 
     driver.quit()
+    write_csv(vagas)
+
+def write_csv(input):
+    headerList = ['Vaga', 'Link']
+    with open('vagas_indeed.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(headerList)
+        writer.writerows(input)
 
 if __name__ == "__main__":
     scrap()
